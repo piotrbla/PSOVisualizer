ab4372fe2555fd1a1fefd38fec37ada0f2ba8d29
PiotrB <piotrb@gmail.com>
2013-12-21 22:54:59 +0100

    Graphics tests

--- PSOVisualizer/Form1.Designer.cs
+++ PSOVisualizer/Form1.Designer.cs
@@ -29,11 +29,46 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1216, 750);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1216, 750);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+
     }
 }
 
--- PSOVisualizer/Form1.cs
+++ PSOVisualizer/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
 
 namespace PSOVisualizer
 {
@@ -6,7 +10,49 @@ namespace PSOVisualizer
     {
         public Form1()
         {
+            randomizer = new Random(DateTime.Now.Millisecond);
             InitializeComponent();
         }
+
+        private Graphics g;
+        private int x;
+        private int y;
+        private int direction = -1;
+        private readonly Random randomizer;
+        private readonly Pen pen = new Pen(Color.Black);
+        readonly List<Tuple<int, int>> points = new List<Tuple<int, int>>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+            timer1.Interval =2;
+            timer1.Start();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            g.DrawLine(pen, 10, 10, 1234, 1000 );
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            direction = randomizer.Next(5) < 4 ? 1 : -1;
+            x += randomizer.Next(3) * direction;
+            direction = randomizer.Next(5) < 4 ? 1 : -1;
+            y += randomizer.Next(3) * direction;
+            points.Add(new Tuple<int, int>(x,y));
+
+            if (points.Count % 50 == 0 )
+                g.Clear(Color.Beige);
+            g.DrawString(points.Count.ToString(),new Font(new FontFamily(GenericFontFamilies.SansSerif),12), new SolidBrush(Color.Black), 500, 100);
+            foreach (var point in points)
+            {
+                g.DrawLine(pen, point.Item1, point.Item2, point.Item1+2, point.Item2+2 );
+            }
+        }
+
+
     }
 }
